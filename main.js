0318f8f582e02345f3e0f8114c93f227499bb37c
BunnyNomics101 <BunnyNomics101@gmail.com>
2022-09-18 22:15:24 +0200

    pro4

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './src/tailwind.css'
+import './tailwind.css'
 import * as THREE from 'three'
 import gsap from 'gsap'
 import countries from './countries.json'
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
+  theme: {},
+  variants: {},
+  plugins: [],
+}
